f6f12dc2b25e57902fe1763b06bec6b17ac01d36
sohamkadu17 <kadusoham91@gmail.com>
2026-08-03 18:16:25 +0530

    Intialized Practice

---
+++ Practice_questions/linklist/first.java
@@ -0,0 +1,8 @@
+package Practice_questions.linklist;
+import java.util.Scanner;
+public class first {
+    public static void main(String[] args) {
+     Scanner sc = new Scanner(System.in);
+     
+    }
+}
