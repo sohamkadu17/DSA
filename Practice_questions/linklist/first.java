b8d4a81c470c5b02bd7cb62f7cd538cd1226379a
sohamkadu17 <kadusoham91@gmail.com>
2026-08-03 19:14:09 +0530

    1st question

--- Practice_questions/linklist/first.java
+++ Practice_questions/linklist/first.java
@@ -1,8 +1,12 @@
 package Practice_questions.linklist;
 import java.util.Scanner;
+// recursive insert in linked list;
+
 public class first {
     public static void main(String[] args) {
      Scanner sc = new Scanner(System.in);
-     
+     int n = sc.nextInt();
+     System.out.println(n);
+    sc.close();
     }
 }
