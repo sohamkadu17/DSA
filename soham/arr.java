467a4e3e21267dceea3f5f8a13af42d408ab3704
sohamkadu17 <kadusoham91@gmail.com>
2026-08-05 22:26:33 +0530

    This is for arrays

--- soham/arr.java
+++ soham/arr.java
@@ -1,5 +1,19 @@
 package soham;
+import java.util.Arrays;
+
 
 public class arr {
-    
+
+    public static void main(String[] args) {
+        int[] arr = new int[6];
+        arr[0] = 1;
+        arr[1] = 2;
+        arr[2] = 3;
+        arr[3] = 4;
+        arr[4] = 5;
+        arr[5] = 6; 
+
+        System.out.println(arr.toString());
+    }
+
 }
