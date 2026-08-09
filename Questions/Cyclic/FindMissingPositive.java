19c44ca9d84a7f326101c35ddc517ebb35397aff
sohamkadu17 <kadusoham91@gmail.com>
2026-08-09 16:49:04 +0530

    LeetCode hard Question

---
+++ Questions/Cyclic/FindMissingPositive.java
@@ -0,0 +1,30 @@
+package Questions.Cyclic;
+
+
+public class FindMissingPositive {
+    public static void main(String[] args) {
+        int[] arr = {3,2,2};
+        int ans =firstMissingPositive(arr);
+        System.out.println("the ans is : "+ ans);
+    }
+
+    public static int firstMissingPositive(int[] nums){
+        int i = 0;
+        while(i<nums.length){
+            int correct = nums[i]-1;
+            if(nums[i] > 0 && nums[i] <= nums.length && nums[correct] != nums[i]){
+                int temp = nums[i];
+                nums[i] = nums[correct];
+                nums[correct] = temp;
+            }else{
+                i++;
+            }
+        }
+        for(i = 0; i<nums.length; i++){
+            if(nums[i] != i+1){
+                return i+1;
+            }
+        }
+        return nums.length +1;
+    }
+}
