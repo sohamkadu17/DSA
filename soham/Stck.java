54e4f0f4dd1cd5c3de2c4484e436907b9ee148c1
sohamkadu17 <kadusoham91@gmail.com>
2026-08-11 00:10:39 +0530

    Que and stack Implementation

---
+++ soham/Stck.java
@@ -0,0 +1,12 @@
+package soham;
+import java.util.Stack;
+public class Stck{
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(9);
+        s.push(2);
+        
+
+        System.out.println(s.pop());
+    }
+}
---
+++ soham/que.java
@@ -0,0 +1,11 @@
+package soham;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+
+public class que {
+    public static void main(String[] args) {
+        Queue<Integer> sc = new LinkedList<>();
+    }
+}
