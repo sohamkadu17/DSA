9e3be3991f14f4f9a7bef2908f575abdf39d8b67
sohamkadu17 <kadusoham91@gmail.com>
2026-08-11 00:24:20 +0530

    Deque implementation

--- soham/que.java
+++ soham/que.java
@@ -2,10 +2,25 @@ package soham;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Deque;
 
 
 public class que {
     public static void main(String[] args) {
         Queue<Integer> sc = new LinkedList<>();
+        sc.add(23);
+        sc.add(21);
+        sc.add(24);
+        sc.add(27);
+        System.out.println(sc.remove());
+        //  remove Poll are also some methods used in que.
+        // Dequeue is used to insert and remove from both the ends of the queue
+
+
+        Deque<Integer> deque = new LinkedList<>();
+        deque.addFirst(23);
+        deque.addLast(24);
+        System.out.println(deque.removeFirst());
+        System.out.println(deque.removeLast());
     }
 }
