376990bcebe7c23a9d42358781f29d39a9998531
sohamkadu17 <kadusoham91@gmail.com>
2026-08-14 23:21:35 +0530

    refactor: enable inline suggestions in settings.json

---
+++ Questions/DailyLeetCode/MaximumLenSubString.java
@@ -0,0 +1,17 @@
+package Questions.DailyLeetCode;
+
+
+public class MaximumLenSubString {
+    public static int maximumLengthSubstring(String s) {
+        int maxLen = 2;
+        int len = 2;
+        while(true)
+        return maxLen;
+    }
+    
+    public static void main(String[] args){
+        String s = "bcbbbcba";
+
+
+    }
+}   
