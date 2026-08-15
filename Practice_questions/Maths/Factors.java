3d227aab21b91d463442601e91190caf5c7b15eb
sohamkadu17 <kadusoham91@gmail.com>
2026-08-16 00:47:42 +0530

    Finding GCD

--- Practice_questions/Maths/Factors.java
+++ Practice_questions/Maths/Factors.java
@@ -2,7 +2,15 @@ package Practice_questions.Maths;
 
 public class Factors {
     public static void main(String [] args){
-        
+        int a = 16;
+        int b = 4;
+        System.out.println(gcd(a,b));
+    }
+    private static int gcd(int a, int b){
+        if(a == 0){
+            return b;
+        }
+        return gcd(a%b , b);
     }
     
 }
