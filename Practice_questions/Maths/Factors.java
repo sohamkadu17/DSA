f39a69db0111ea58d63f4e8c0a42e509d57ea980
sohamkadu17 <kadusoham91@gmail.com>
2026-08-16 01:57:50 +0530

    added LCM

--- Practice_questions/Maths/Factors.java
+++ Practice_questions/Maths/Factors.java
@@ -4,8 +4,12 @@ public class Factors {
     public static void main(String [] args){
         int a = 16;
         int b = 4;
+        System.out.println(LCM(a,b));
         System.out.println(gcd(a,b));
     }
+    private static int LCM(int a,int b){
+        return a*b/gcd(a,b);
+    } 
     private static int gcd(int a, int b){
         if(a == 0){
             return b;
