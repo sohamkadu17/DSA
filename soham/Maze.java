10099658286907ffbc720eb1a2f09a8d560ee993
sohamkadu17 <kadusoham91@gmail.com>
2026-08-19 01:45:58 +0530

    Maze Problem Returing arraylist

--- soham/Maze.java
+++ soham/Maze.java
@@ -1,9 +1,12 @@
 package soham;
 
+import java.util.ArrayList;
+
 public class Maze {
     public static void main(String[] args) {
-        System.out.println(count(2, 3));
-        path("", 2, 3);
+        System.out.println(count(4, 4));
+        // path("", 4, 4);
+        System.out.println(pathRet("", 4, 4));
     }
 
     private static int count(int r, int c){
@@ -28,6 +31,26 @@ public class Maze {
         if(c> 1){
             path(p+"R",r,c-1);
         }
+    }
+
+    private static ArrayList<String> pathRet(String p, int r , int c ){
+        if(r==1 && c ==1 ){
+            ArrayList<String>list = new ArrayList<>();
+            list.add(p);
+            return list;
+        }
+        ArrayList<String> list = new ArrayList<>();
+        if(r>1){
+            list.addAll(pathRet(p+"D",r-1,c));
+
+        }
+        if(c>1){
+            list.addAll(pathRet(p+"R", r,c-1));
+        }
+
+
+
+        return list;
     }
 
 }
