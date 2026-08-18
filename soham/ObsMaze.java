799e5150071f05bf56fa054d5ff67396d8498a1a
sohamkadu17 <kadusoham91@gmail.com>
2026-08-19 02:03:29 +0530

    Obstruction within the maze

---
+++ soham/ObsMaze.java
@@ -0,0 +1,31 @@
+package soham;
+
+public class ObsMaze {
+    public static void main(String[] args){
+        boolean[][] matrix = {
+                {true,true,true},
+                {true,false,true},
+                {true,true,true}
+                };
+
+            pathObs("",matrix,0,0);
+                
+        
+    }
+
+    private static void pathObs(String p, boolean[][] matrix, int r,int c){
+        if(r == matrix.length-1&& c == matrix[0].length-1){
+            System.out.println(p);
+            return;
+        } 
+        if(!matrix[r][c]) {
+            return;
+        }
+        if(r<matrix.length-1){ 
+            pathObs(p+"D",matrix,r+1,c);
+        }
+        if(c<matrix[0].length -1){
+            pathObs(p+"R",matrix,r,c+1);
+        }
+    }
+}
